c8a7c338b9d8c2e9907d794565ccf3edbd1889be
akolomytsev <kolm.87@mail.ru>
2022-11-01 22:20:06 +0600

    Задача про часы

---
+++ src/main/java/ru/kolomytsev/test/DigitalWatches.java
@@ -0,0 +1,44 @@
+package ru.kolomytsev.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Scanner;
+
+//Цифровые часы
+//Цифровые часы отображают время в формате ч:мм:сс (от 0:00:00 до 23:59:59),
+// т.е. сначала идет количество часов, затем идет двузначное количество минут,
+// затем двузначное число секунд. При необходимости количество минут и секунд дополняется нулями до двузначного числа.
+//С начала дня прошло N секунд. Выведите то, что будут отображать часы.
+//Формат входных данных
+//Учитывая натуральное число N на входе, не превосходящее 10^7 (10000000).
+//Пример ввода 1:
+//3602
+//Пример вывода 1:
+//1:00:02
+//Пример ввода 2:
+//129700
+//Пример вывода 2:
+//12:01:40
+public class DigitalWatches {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Введите количество секунд");
+        int a = userInput();
+        while (a > 0 && a <= 10000000){
+
+        }
+    }
+    private static int userInput() {
+        //Scanner sc = new Scanner(System.in);
+        int i;
+        while (true) {
+            try {
+                i = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный ввод!");
+            }
+        }
+        return i;
+    }
+}
